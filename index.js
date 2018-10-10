920d9eadfbecd4929eebc279537fecdcecd6bfbd
Yixuan Wang <ywang0506@gmail.com>
2018-10-10 15:47:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,74 +1,53 @@
 let store = {drivers: [], trips: [], passengers:[]}
 
 let driverId = 0
-class Driver {
-  constructor(name) {
+class Driver{
+  constructor(name){
     this.id = ++driverId;
     this.name = name;
     store.drivers.push(this);
   }
-  // items() {
-  //       return store.items.filter(
-  //           function(item) {
-  //               return item.userId === this.id;
-  //           }.bind(this)
-  //       );
-  //   }
   trips(){
-    return store.trips.filter(
-      function(trip){
-        return trip.driverId === this.id;
-      }.bind(this)
-    )
+    return store.trips.filter(trip => trip.driverId ==this.id)
   }
+
   passengers(){
-    return this.trips().map(
-      trip => {
-        return trip.passenger();
-    })
+    return this.trips().map(trip=> trip.passenger())
   }
+
+
 }
-
-
-let passengerId = 0
-class Passenger {
+let passengerId= 0
+class Passenger{
   constructor(name){
     this.id = ++passengerId;
     this.name = name;
     store.passengers.push(this);
-
   }
+
   trips(){
-    return store.trips.filter(
-      trip => trip.passengerId === this.id
-    )
+    return store.trips.filter(trip=> trip.passengerId == this.id)
   }
 
- drivers(){
-   return this.trips().map(
-    trip=> trip.driver()
-   )
- }
+  drivers(){
+    return this.trips().map(trip=> trip.driver())
+  }
 }
 
-
 let tripId = 0
-class Trip {
+class Trip{
   constructor(driver, passenger){
     this.id = ++tripId;
-    this.driverId = driver.id
-    this.passengerId = passenger.id;
     store.trips.push(this);
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
   }
+
   passenger(){
-    return store.passengers.find(
-      function(passenger) {
-        return passenger.id === this.passengerId;
-      }.bind(this)
-    )
+    return store.passengers.find(passenger => passenger.id == this.passengerId)
   }
+
   driver(){
-    return store.drivers.find(
-      driver => driver.id === this.driverId)
+    return store.drivers.find(driver=> driver.id == this.driverId)
   }
 }
